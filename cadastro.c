d0a69ebdaeee0158798058cc9f52d4be7d1ff137
luis guilherme <tete42449@gmail.com>
2024-05-24 17:13:19 -0300

    melhoria total

--- cadastro.c
+++ cadastro.c
@@ -11,10 +11,10 @@ char NomeCompletoProf[10], MatriculaProf[10], CPFprof[10], DataNascimentoProf[10
 // disciplina
 char NomedaDisciplina[10], curso[10], CodigoDisciplina[10];
 // curso
-char CodigoCurso[10] /*a variavel curso ? utilizada aqui tbm*/;
+char CodigoCurso[10] /*a variavel curso é utilizada aqui tbm*/;
 // boletim
-char CodigoBoletim[10], DataBoletim[10], notas[10], MediadasNotas[10],materiaprof[10];
-// opera??es dos menus
+char CodigoBoletim[10], DataBoletim[10], notas[10], MediadasNotas[10], materiaprof[10];
+// operações dos menus
 int OpMenuPincipal, OpSubMenuCad, OpSubMenuImpressao, operacao;
 //  caso necessario acrescentar novas variaveis
 
@@ -29,11 +29,11 @@ void menuprincipal()
     printf("|==========================================================================|\n");
     printf("|                            Cadastro de notas                             |\n");
     printf("+--------------------------------------------------------------------------+\n");
-    printf("|         1-Cadastro                 |         2-Impress?o                 |\n");
+    printf("|         1-Cadastro                 |         2-Impressão                 |\n");
     printf("+--------------------------------------------------------------------------+\n");
-    printf("|         3-Conte?dos                |         4-Notas                     |\n");
+    printf("|         3-Conteúdos                |         4-Notas                     |\n");
     printf("===========================================================================+\n");
-    printf("Informe a opera??o ou 5 para sair.\n");
+    printf("Informe a operação ou 5 para sair.\n");
     scanf("%d", &OpMenuPincipal);
     switch (OpMenuPincipal)
     {
@@ -63,7 +63,7 @@ void submenucad()
     printf("+--------------------------------------------------------------------------+\n");
     printf("|                    5- para voltar para o menu principal                  |\n");
     printf("===========================================================================+\n");
-    printf("Informe a opera??o.\n");
+    printf("Informe a operação.\n");
     scanf("%d", &OpSubMenuCad);
     switch (OpSubMenuCad)
     {
@@ -83,11 +83,11 @@ void submenucad()
         printf("informe o curso do aluno:\n");
         scanf("%s", curso);
         fprintf(arquivo, "+------------------Cad.aluno----------------+\n");
-        fprintf(arquivo, "o nome do aluno ?:%s\n", NomeCompletoAluno);
-        fprintf(arquivo, "o CPF ?:%s\n", CPFaluno);
-        fprintf(arquivo, "a matricula do aluno ?:%s\n", MatriculaAluno);
-        fprintf(arquivo, "a data de nascimento do aluno ?:%s\n", DataNascimentoAluno);
-        fprintf(arquivo, "o curso do aluno ?:%s\n", cursoaluno);
+        fprintf(arquivo, "o nome do aluno é:%s\n", NomeCompletoAluno);
+        fprintf(arquivo, "o CPF é:%s\n", CPFaluno);
+        fprintf(arquivo, "a matricula do aluno é:%s\n", MatriculaAluno);
+        fprintf(arquivo, "a data de nascimento do aluno é:%s\n", DataNascimentoAluno);
+        fprintf(arquivo, "o curso do aluno é:%s\n", cursoaluno);
         submenucad();
 
         break;
@@ -105,10 +105,10 @@ void submenucad()
         printf("informe a materia:\n");
         scanf("%s", &materiaprof);
         fprintf(arquivo, "+------------------Cad.Prof-----------------+\n");
-        fprintf(arquivo, "o nome do professor ?:%s\n", NomeCompletoProf);
-        fprintf(arquivo, "o CPF ?:%s\n", CPFprof);
-        fprintf(arquivo, "a matricula do professor ?:%s\n", MatriculaProf);
-        fprintf(arquivo, "a data de nascimento do professor ?:%s\n", DataNascimentoProf);
+        fprintf(arquivo, "o nome do professor é:%s\n", NomeCompletoProf);
+        fprintf(arquivo, "o CPF é:%s\n", CPFprof);
+        fprintf(arquivo, "a matricula do professor é:%s\n", MatriculaProf);
+        fprintf(arquivo, "a data de nascimento do professor é:%s\n", DataNascimentoProf);
         fprintf(arquivo, "o nome da materia:%s\n", materiaprof);
         submenucad();
 
@@ -119,8 +119,8 @@ void submenucad()
         printf("informe o codigo da disciplina:\n");
         scanf("%s", &CodigoDisciplina);
         fprintf(arquivo, "+------------------Cad.discip--------------+\n");
-        fprintf(arquivo, "O nome da disciplina ?:%s\n", NomedaDisciplina);
-        fprintf(arquivo, "O c?digo da disciplina ?:%s\n", CodigoDisciplina);
+        fprintf(arquivo, "O nome da disciplina é:%s\n", NomedaDisciplina);
+        fprintf(arquivo, "O código da disciplina é:%s\n", CodigoDisciplina);
 
         submenucad();
         break;
@@ -128,7 +128,7 @@ void submenucad()
         printf("informe o curso: \n");
         scanf("%s", &curso);
         fprintf(arquivo, "+------------------Cad.Curso--------------+\n");
-        fprintf(arquivo, "o curso ?:%s\n", curso);
+        fprintf(arquivo, "o curso é:%s\n", curso);
         fclose(arquivo);
         break;
     case 5:
@@ -142,15 +142,15 @@ void submenuimpressao()
 {
 
     printf("|==========================================================================|\n");
-    printf("|                              impress?o de dados                          |\n");
+    printf("|                              impressão de dados                          |\n");
     printf("+--------------------------------------------------------------------------+\n");
-    printf("|         1-Alunos e Curso                 |          2-conte?dos          |\n");
+    printf("|         1-Alunos e Curso                 |          2-conteúdos          |\n");
     printf("+--------------------------------------------------------------------------+\n");
     printf("|         3-Disciplina e Professor         |          4-boletim            |\n");
     printf("+--------------------------------------------------------------------------+\n");
     printf("|         5-menu principal                 |          6-cadastro geral     |\n");
     printf("===========================================================================+\n");
-    printf("Informe a opera??o:\n");
+    printf("Informe a operação:\n");
     scanf("%d", &OpSubMenuImpressao);
     switch (OpSubMenuImpressao)
     {
