f6756799ee916e4b45fce7120482dad1100633f1
luis guilherme <tete42449@gmail.com>
2024-05-24 15:37:46 -0300

    melhorias

--- cadastro.c
+++ cadastro.c
@@ -5,7 +5,7 @@
 // arquivos
 FILE *arquivo;
 // dados aluno
-char MatriculaAluno[10], CPFaluno[10], DataNascimentoAluno[10], NomeCompletoAluno[10];
+char MatriculaAluno[10], CPFaluno[10], DataNascimentoAluno[10], NomeCompletoAluno[10], cursoaluno[10];
 // dados professor
 char NomeCompletoProf[10], MatriculaProf[10], CPFprof[10], DataNascimentoProf[10];
 // disciplina
@@ -13,13 +13,18 @@ char NomedaDisciplina[10], curso[10], CodigoDisciplina[10];
 // curso
 char CodigoCurso[10] /*a variavel curso é utilizada aqui tbm*/;
 // boletim
-char CodigoBoletim[10], DataBoletim[10], notas[10], MediadasNotas[10];
+char CodigoBoletim[10], DataBoletim[10], notas[10], MediadasNotas[10],materiaprof[10];
 // operações dos menus
 int OpMenuPincipal, OpSubMenuCad, OpSubMenuImpressao, operacao;
 //  caso necessario acrescentar novas variaveis
 
 void menuprincipal()
 {
+    arquivo = fopen("C://temp//notas.txt", "w+");
+    if (arquivo == 0)
+    {
+        printf("falha ao visualizar o arquivo.\n");
+    }
 
     printf("|==========================================================================|\n");
     printf("|                            Cadastro de notas                             |\n");
@@ -63,70 +68,68 @@ void submenucad()
     switch (OpSubMenuCad)
     {
     case 1:
-        arquivo = fopen("C://temp//notas.txt", "w+");
-        if (arquivo == 0)
-        {
-            printf("falha ao visualizar o arquivo.\n");
-        }
-        else
-        {
-            printf("arquivo encontrado com sucesso\n");
-            system("Pause");
-            printf("nos dados:CPF,data de nascimento e matricula digite apenas os numeros.\n");
-            printf("informe o nome do aluno:EX:\"pedro\" \n");
-            scanf("%s", &NomeCompletoAluno);
-            printf("informe o CPF  do aluno: EX:000.000.000-00\n");
-            scanf("%s", &CPFaluno);
-            printf("informe a matricula do aluno:EX:000.000\n");
-            scanf("%s", &MatriculaAluno);
-            printf("informe a data de nascimento do aluno:EX:00/00/0000\n");
-            scanf("%s", &DataNascimentoAluno);
-            printf("informe o curso:\n");
-            scanf("%s", curso);
-            fprintf(arquivo, "o nome do aluno é:%s\n", NomeCompletoAluno);
-            fprintf(arquivo, "o CPF é:%s\n", CPFaluno);
-            fprintf(arquivo, "a matricula do aluno é:%s\n", MatriculaAluno);
-            fprintf(arquivo, "a data de nascimento do aluno é:%s\n", DataNascimentoAluno);
-            fprintf(arquivo, "o curso é:%s\n", curso);
-            fclose(arquivo);
-        }
+
+        printf("arquivo encontrado com sucesso\n");
+        system("Pause");
+        printf("nos dados:CPF,data de nascimento e matricula digite apenas os numeros.\n");
+        printf("informe o nome do aluno:EX:\"pedro\" \n");
+        scanf("%s", &NomeCompletoAluno);
+        printf("informe o CPF  do aluno: EX:000.000.000-00\n");
+        scanf("%s", &CPFaluno);
+        printf("informe a matricula do aluno:EX:000.000\n");
+        scanf("%s", &MatriculaAluno);
+        printf("informe a data de nascimento do aluno:EX:00/00/0000\n");
+        scanf("%s", &DataNascimentoAluno);
+        printf("informe o curso do aluno:\n");
+        scanf("%s", curso);
+        fprintf(arquivo, "+------------------Cad.aluno----------------+\n");
+        fprintf(arquivo, "o nome do aluno é:%s\n", NomeCompletoAluno);
+        fprintf(arquivo, "o CPF é:%s\n", CPFaluno);
+        fprintf(arquivo, "a matricula do aluno é:%s\n", MatriculaAluno);
+        fprintf(arquivo, "a data de nascimento do aluno é:%s\n", DataNascimentoAluno);
+        fprintf(arquivo, "o curso do aluno é:%s\n", cursoaluno);
+        submenucad();
+
         break;
     case 2:
-        arquivo = fopen("C://temp//notas.txt", "w+");
-        if (arquivo == 0)
-        {
-            printf("falha ao visualizar o arquivo.\n");
-        }
-        else
-        {
-            printf("nos dados:CPF,data de nascimento e matricula digite apenas os numeros.\n");
-            printf("insira os dados do professor: EX:\"pedro\"");
-            scanf("%s", &NomeCompletoProf);
-            printf("informe o CPF  do professor: EX:000.000.000-00\n");
-            scanf("%s", &CPFprof);
-            printf("informe a matricula do professor: EX:000.000\n");
-            scanf("%s", &MatriculaProf);
-            printf("informe a data de nascimento do professor: EX:00/00/0000\n");
-            scanf("%s", &DataNascimentoProf);
-            printf("informe o curso:\n");
-            scanf("%s", curso);
-            fprintf(arquivo, "o nome do professor é:%s\n", NomeCompletoProf);
-            fprintf(arquivo, "o CPF é:%s\n", CPFprof);
-            fprintf(arquivo, "a matricula do professor é:%s\n", MatriculaProf);
-            fprintf(arquivo, "a data de nascimento do professor é:%s\n", DataNascimentoProf);
-            fprintf(arquivo, "o curso é:%s\n", curso);
-            fclose(arquivo);
-        }
+
+        printf("nos dados:CPF,data de nascimento e matricula digite apenas os numeros.\n");
+        printf("insira os dados do professor: EX:\"pedro\"\n");
+        scanf("%s", &NomeCompletoProf);
+        printf("informe o CPF  do professor: EX:000.000.000-00\n");
+        scanf("%s", &CPFprof);
+        printf("informe a matricula do professor: EX:000.000\n");
+        scanf("%s", &MatriculaProf);
+        printf("informe a data de nascimento do professor: EX:00/00/0000\n");
+        scanf("%s", &DataNascimentoProf);
+        printf("informe a materia:\n");
+        scanf("%s", &materiaprof);
+        fprintf(arquivo, "+------------------Cad.Prof-----------------+\n");
+        fprintf(arquivo, "o nome do professor é:%s\n", NomeCompletoProf);
+        fprintf(arquivo, "o CPF é:%s\n", CPFprof);
+        fprintf(arquivo, "a matricula do professor é:%s\n", MatriculaProf);
+        fprintf(arquivo, "a data de nascimento do professor é:%s\n", DataNascimentoProf);
+        fprintf(arquivo, "o nome da materia:%s\n", materiaprof);
+        submenucad();
+
         break;
     case 3:
         printf("informe a disciplina:\n");
-        scanf("%s", NomedaDisciplina);
+        scanf("%s", &NomedaDisciplina);
         printf("informe o codigo da disciplina:\n");
-        scanf("%s", CodigoDisciplina);
+        scanf("%s", &CodigoDisciplina);
+        fprintf(arquivo, "+------------------Cad.discip--------------+\n");
+        fprintf(arquivo, "O nome da disciplina é:%s\n", NomedaDisciplina);
+        fprintf(arquivo, "O código da disciplina é:%s\n", CodigoDisciplina);
 
+        submenucad();
         break;
     case 4:
-        printf("informe o curso \n");
+        printf("informe o curso: \n");
+        scanf("%s", &curso);
+        fprintf(arquivo, "+------------------Cad.Curso--------------+\n");
+        fprintf(arquivo, "o curso é:%s\n", curso);
+        fclose(arquivo);
         break;
     case 5:
         system("cls");
